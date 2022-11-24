eedd3dd27dad9b2e5ea0c60bb303b3eaaa244cab
Sudeep Dahal <sudeepdahal75@gmail.com>
2022-11-24 20:57:09 +0545

    Update Textbox.js

--- src/components/Textbox.js
+++ src/components/Textbox.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Textbox = () => {
   return (
     <div className="text-box">
-        <h3>SKILLS</h3>
-     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime asperiores perspiciatis facilis, repudiandae veniam id vel fuga quibusdam expedita dolorum quam consectetur, blanditiis tempore commodi assumenda iure. Ducimus unde quod praesentium, sed quae esse deleniti deserunt necessitatibus repudiandae voluptate cumque vel sunt nisi natus minus temporibus sit impedit cum accusantium.</p>
+      <h3>SKILLS123</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
+        asperiores perspiciatis facilis, repudiandae veniam id vel fuga
+        quibusdam expedita dolorum quam consectetur, blanditiis tempore commodi
+        assumenda iure. Ducimus unde quod praesentium, sed quae esse deleniti
+        deserunt necessitatibus repudiandae voluptate cumque vel sunt nisi natus
+        minus temporibus sit impedit cum accusantium.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Textbox
+export default Textbox;
